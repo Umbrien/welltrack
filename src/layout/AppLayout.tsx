fe69aeba35a89ed2054dba16232b81d22acb6907
Serhii <kaluhin.dev@gmail.com>
2024-07-14 13:13:39 +0300

    navigation

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,12 +1,48 @@
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "../components/ui/tooltip";
+import { TableProperties, CalendarDays, CirclePlus } from "lucide-react";
+import { Link } from "wasp/client/router";
 import "../Main.css";
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col gap-3 min-h-dvh">
-      <nav className="bg-primary p-4">
-        <img src="/logo.svg" alt="logo" className="h-8" />
-      </nav>
-      {children}
-    </div>
+    <TooltipProvider>
+      <div className="flex flex-col gap-3 min-h-dvh">
+        <nav className="bg-primary p-4 flex items-center justify-between">
+          <img src="/logo.svg" alt="logo" className="h-8" />
+          <div className="flex gap-4">
+            <Tooltip>
+              <TooltipTrigger>
+                <Link to="/day-analysis">
+                  <TableProperties size={24} color="white" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>Week view</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger>
+                <Link to="/month-analysis">
+                  <CalendarDays size={24} color="white" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>Month view</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger>
+                <Link to="/">
+                  <CirclePlus size={24} color="white" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>Add record</TooltipContent>
+            </Tooltip>
+          </div>
+        </nav>
+        {children}
+      </div>
+    </TooltipProvider>
   );
 }
